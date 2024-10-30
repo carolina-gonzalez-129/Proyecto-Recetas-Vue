a0f78ad4035cdd89b951506b034aa9c227587bf3
Carolina <carogschiappapietra@campus.fmed.uba.ar>
2024-10-30 18:32:53 -0300

    Se añadió en el store de pinia un archivo global en el que se configuró el state en el que se grabara el activeUsername (de capital importancia para vincularlo con quien es el autor de la receta a los fines de que sea este el único que cuente con los permisos suficientes para modificar u elmnar la misma), dos actions una para establecer el activeUsername y otra ara el logout(se añadió para esto un componente de logout para cerrar la sesión que invoca a un metodo en global), y dos getters uno para obtener el nombre del usuario y otro para saber si esta logeado (se usara este ult por ej para q el navbar muestre lo de añadir receta /lista de receta solo si el usuario esta logeado, e inversamente si no lo esta se le mostrara q inicie sesion o se registre)  Queda mejorar la presentacion con bootstrap

---
+++ mi-proyecto/src/components/Logout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container mt-5">
+    <button @click="logoutUser" class="btn btn-danger mt-3">Cerrar sesion</button>
+  </div>
+</template>
+<script>
+import {useGlobalStore} from "../../store/global.js";
+export default {
+  data() {
+    return {
+      globalStore: useGlobalStore(),
+    }
+  },
+  methods:{
+    logoutUser() {
+      this.globalStore.logout();
+      alert('Se cerro tu sesion');
+      this.$router.push('/');
+    }
+  }
+};
+</script>
+<style scoped>
+
+</style>
---
+++ mi-proyecto/store/global.js
@@ -0,0 +1,23 @@
+import {defineStore, setActivePinia} from 'pinia';
+
+export const useGlobalStore = defineStore('global', {
+    state: () => ({
+        activeUsername: ''
+    }),
+    actions: {
+        setActiveUsername(username) {
+            this.activeUsername = username;
+        },
+        logout(){
+            this.activeUsername = '';
+        }
+    },
+    getters: {
+        getActiveUsername() {
+            return this.activeUsername;
+        },
+        isUserLoggedIn(state) {
+            return !!state.activeUsername;
+        }
+    }
+});
