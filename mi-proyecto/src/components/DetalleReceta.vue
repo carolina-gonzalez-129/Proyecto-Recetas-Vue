d3e887bad4bf10adf3481ffbce9b091573077bb9
Carolina <carogschiappapietra@campus.fmed.uba.ar>
2024-11-18 00:55:06 -0300

    se añadio una modificacion para que si el user es admin pueda ver la cantidad de visitas de una receta (medida x los clicks q recibio)

--- mi-proyecto/src/components/DetalleReceta.vue
+++ mi-proyecto/src/components/DetalleReceta.vue
@@ -38,6 +38,10 @@
         >
           Eliminar
         </a>
+
+        <a v-if="receta.id && globalStore.getActiveUsername === 'admin'">
+          {{ receta.contador_clicks }} visitas
+        </a>
       </section>
     </div>
   </div>
